e495782483712bccbfdc3f8247054081e5e9d776
YOUR FULL NAMEomarali1997 <omar.aziz.9718@gmail.com>
2022-09-04 23:22:22 +0300

    lab11

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+import Carousel from 'react-bootstrap/Carousel';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -10,17 +12,45 @@ class BestBooks extends React.Component {
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
+  componentDidMount = () => {
+    axios
+    .get(`http://localhost:3001/books`)
+    .then(result => {
+      this.setState ({
+        books : result.data
+      })
+
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+  }
+
+
   render() {
-
     /* TODO: render all the books in a Carousel */
-
     return (
       <>
+      
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
+        {this.state.books.length ? (<Carousel>{
+          this.state.books.map(item => {
+            return (
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://wallpaperaccess.com/full/6875178.jpg"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>{item.title}</h3>
+          <p>{item.description}</p>
+          <h3>{item.status}</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+    )
+          })
+         } </Carousel>) : (
           <h3>No Books Found :(</h3>
         )}
       </>
